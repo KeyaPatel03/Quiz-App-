90dbc945801c8bce2d890e68a6ba7e7cecf0a0bf
KeyaPatel03 <97663842+KeyaPatel03@users.noreply.github.com>
2022-05-14 18:49:11 +0530

    Update game.js

--- game.js
+++ game.js
@@ -5,10 +5,10 @@ const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
 let currentQuestion = {}
-let acceptingAnswers = true ;
-let score = 0;
+let acceptingAnswers = true 
+let score = 0
 let questionCounter = 0
-let availableQuestions =  []
+let availableQuestions = []
 
 let questions = [
     {
@@ -49,10 +49,10 @@ const SCORE_POINTS = 100
 const MAX_QUESTIONS = 4
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
 }
 
 getNewQuestion = () => {
@@ -61,8 +61,8 @@ getNewQuestion = () => {
         return window.location.assign('/end.html')
     }
     questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
 
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex];
@@ -79,7 +79,7 @@ getNewQuestion = () => {
 
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
-        if(acceptingAnswers!=true) return
+        if(!acceptingAnswers) return
 
         acceptingAnswers = false
         const selectedChoice = e.target
@@ -94,13 +94,13 @@ choices.forEach(choice => {
         selectedChoice.parentElement.classList.add(classToApply)
 
         setTimeout(() => {
-            selectedChoice.parentEement.classList.remove(classToApply)
+            selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
         }, 1000)
     })
 })
 incrementScore = num => {
-    score = score + num
+    score += num
     scoreText.innerText = score
 }
 
