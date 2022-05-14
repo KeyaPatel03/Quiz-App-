6316580130fcd7e539ba8fd907fd2849a5902769
KeyaPatel03 <97663842+KeyaPatel03@users.noreply.github.com>
2022-05-14 18:43:47 +0530

    Update end.js

--- end.js
+++ end.js
@@ -1,7 +1,7 @@
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector("#finalScore")
-const mostRecentScore = document.querySelector('#mostReentScore')
+const mostRecentScore = localStorage('mostRecentScore')
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
